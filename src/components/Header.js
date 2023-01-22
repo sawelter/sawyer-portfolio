e656553e712bfc3318ea32b9d91dc400f4536574
Sawyer <sawyer.welter@gmail.com>
2023-01-22 12:24:40 -0600

    Header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 export default function Header (props) {
 
     return (
-        <div>
-            
-        </div>
+        <header>
+            <h1>Sawyer's Portfolio</h1>
+            <nav>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Projects</a>
+                <a href="">Contact</a>
+            </nav>
+        </header>
     )
 }
